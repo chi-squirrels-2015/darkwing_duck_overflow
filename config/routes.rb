35d44d7a2db6e812b7b345d7a3d5b93dd048bb92
hanieldanson <hanson.e.daniel@gmail.com>
2015-02-26 16:28:21 -0600

    update answer routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :answers, only: [:index, :show]
+  resources :answers, only: [:show, :new, :create, :update, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
