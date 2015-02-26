95fc661039ee1c580329096a59671ec076d221b5
Andrew Irwin <andrew.irwin33@gmail.com>
2015-02-26 17:02:20 -0600

    added question models, migrations, and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -19,13 +19,6 @@ ActiveRecord::Schema.define(version: 20150226204548) do
   enable_extension "plpgsql"
 
 
-  create_table "questions", force: true do |t|
-    t.string   "title"
-    t.text     "content"
-    t.integer  "vote_count"
-    t.integer  "user_id"
-  end
-
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -47,6 +40,16 @@ ActiveRecord::Schema.define(version: 20150226204548) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
+
+
+  create_table "questions", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "vote_count"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 
 end
