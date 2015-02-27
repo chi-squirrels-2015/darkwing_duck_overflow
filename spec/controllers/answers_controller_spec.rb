c7d33af2724a0d3a327085d2d83b0b75f4789a5e
Andrew Irwin & Daniel Hanson & Shiraz Sherwani <pair+airwin33.hanieldanson.pacoguy@devbootcamp.com>
2015-02-27 14:34:17 -0600

    answer controller tests passing and answer form added

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,60 +5,46 @@ describe AnswersController do
   let!(:question) { Question.create!(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, vote_count: 5, user: user) }
   let!(:answer) { Answer.create!(content: Faker::Lorem.paragraph, vote_count: 20, user: user) }
 
-  # describe "GET #index" do
-  #   it "assigns all answers as @answers" do
-  #     get :index
-  #     expect(assigns(:answers)).to eq(Answer.all)
-  #     # Hint: You can gain access to any instance variable in your
-  #     # controller with assigns(:ivar_name). Don't forget to make
-  #     # a request!
-  #   end
-  # end
-
-  # describe "GET #show" do
-  #   it "assigns the requested answer as @answer" do
-  #     get :show, { id: answer.to_param }
-  #     expect(assigns(:answer)).to eq(answer)
-  #   end
-  # end
-
   describe "GET #new" do
       it 'should generate a new instance of answer' do
-      get :new
+      get :new, :question_id => question.id
         expect(assigns(:answer)).to be_an_instance_of(Answer)
     end
   end
 
-
-
-  describe "POST create" do
+  describe "POST #create" do
     context "when valid params are passed" do
       it "creates a new Answer" do
-        expect{ post :create, {answer: {content: 'terrible solution, bro.'} } }.to change{Answer.count}.by(1)
+        expect{ post :create, {question_id: question.id, answer: {content: 'terrible solution, bro.'} } }.to change{Answer.count}.by(1)
       end
 
       it "assigns a newly created game as @answer" do
-        post :create, {answer: {content: 'terrible solution, bro.'} }
+        post :create, {question_id: question.id, answer: {content: 'terrible solution, bro.'} }
         expect(assigns(:answer)).to be_an_instance_of(Answer)
       end
 
 # Probably don't need a redirect since this will be ajax?
       it "redirects to the created game" do
-        post :create, {answer: {content: 'terrible solution, bro'} }
+        post :create, {question_id: question.id, answer: {content: 'terrible solution, bro'} }
         expect(response.status).to eq(302)
       end
     end
   end
 
+  # describe "PUT #update" do
+  #     it 'should update the content of an answer' do
+  #   end
+  # end
+
   describe "DELETE #destroy" do
     it "assigns the requested answer as @answer" do
-      delete :destroy, { id: answer.to_param }
+      delete :destroy, {question_id: question.id, id: answer.to_param }
       expect(assigns(:answer)).to eq(answer)
     end
 
     it "destroys the requested answer" do
       expect {
-        delete :destroy, id: answer.id
+        delete :destroy, question_id: question.id, id: answer.id
       }.to change(Answer, :count).by(-1)
     end
   end
