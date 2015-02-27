fe265527f9b70be6a627d0b8c811a7eb9159dbd3
PacoGuy <sfsherwani@gmail.com>
2015-02-27 11:41:15 -0600

    Add user_spec.rb RSpec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { User.new }
+
+  context 'validations' do
+    context 'email' do
+      it 'should not allow a non-unique email' do
+        user1 = User.new(email:"fake@fake.com", password: "fakefake")
+        user1.save!
+        user2 = User.new(email:"fake@fake.com", password: "fakefake")
+        user2.save
+        expect(user2.errors[:email]).to_not be_empty
+      end
+
+      it 'should not allow a non properly formatted email' do
+        user.email = "fakeandbreakthings42"
+        user.save
+        expect(user.errors.messages[:email].length).to eq(1)
+      end
+
+      it 'should accept a properly formatted, unique email' do
+        user.email = "fake@working.org"
+        user.save
+        expect(user.errors[:email]).to be_empty
+      end
+    end
+
+    context 'password' do
+      it 'should not accept a password with fewer than 8 characters' do
+        user.password = "break"
+        user.save
+        expect(user.errors[:password]).to_not be_empty
+      end
+
+      it 'should not allow password to be blank' do
+        user.password = ""
+        user.save
+        expect(user.errors[:password]).to_not be_empty
+      end
+
+      it 'should accept a password of 8 or more characters' do
+        user.password = "working1"
+        user.save
+        expect(user.errors[:password]).to be_empty
+      end
+    end
+  end
 end
