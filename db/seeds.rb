f97e5a4c0f7e15a0e049fa31b1e67d4be33e8a4a
hanieldanson <hanson.e.daniel@gmail.com>
2015-03-01 10:33:25 -0600

    update seed for greater # of emails and greater # of votes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,31 +7,27 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-#### NEEDS USER AND QUESTION ARGUMENTS)
-
-
-
-100.times do
-  User.create!(username: Faker::Internet.user_name,
-               email: Faker::Internet.free_email,
+400.times do
+  User.create!(username: [(Faker::Internet.user_name + Faker::Number.number(5)), (Faker::Internet.user_name + Faker::Number.number(4)), (Faker::Hacker.noun + Faker::Number.number(4))].sample,
+               email: [Faker::Internet.free_email, Faker::Internet.email, Faker::Internet.safe_email].sample,
                password: 'password')
 end
 
 users = User.all
 
 
-20.times do
+30.times do
   Question.create!(title: Faker::Hacker.say_something_smart.gsub(/[!.]$/, '?'),
                    content: Faker::Hacker.say_something_smart.gsub(/[!.]$/, '?') + ' ' + Faker::Hacker.say_something_smart.gsub(/[!.]$/, '?') + ' ' + Faker::Hacker.say_something_smart.gsub(/[!.]$/, '?'),
                    user: users.sample,
-                   created_at: rand(1.year).ago,
-                   updated_at: rand(1.year).ago)
+                   created_at: Faker::Time.between(6.months.ago, Time.now, :all),
+                   updated_at: Faker::Time.between(6.months.ago, Time.now, :all))
 end
 
 questions = Question.all
 
 
-100.times do
+120.times do
   Answer.create!(content: Faker::Hacker.say_something_smart + ' ' + Faker::Hacker.say_something_smart + ' ' + Faker::Hacker.say_something_smart,
                  user: users.sample,
                  question: questions.sample)
@@ -40,23 +36,23 @@ end
 answers = Answer.all
 
 
-10.times do
+50.times do
   Vote.create!(voteable: questions.sample,
                voter: users.sample)
 end
 
-10.times do
+150.times do
   Vote.create!(voteable: answers.sample,
                voter: users.sample)
 end
 
-20.times do
+25.times do
   Comment.create!(content: Faker::Hacker.say_something_smart,
                   commentable: questions.sample,
                   commenter: users.sample)
 end
 
-10.times do
+15.times do
   Comment.create!(content: Faker::Hacker.say_something_smart,
                   commentable: answers.sample,
                   commenter: users.sample)
